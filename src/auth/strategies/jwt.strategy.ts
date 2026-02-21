1fdc6c20b128379f7b5a9f6823015b1569c8a3f8
awoladhossain <awoladh04@gmail.com>
2026-02-21 14:30:39 +0600

    guards jwt refresh

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+import { PassportStrategy } from '@nestjs/passport';
+import { Model } from 'mongoose';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User, UserDocument } from '../schemas/user.schema';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    config: ConfigService,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get<string>('JWT_ACCESS_SECRET')!,
+    });
+  }
+
+  async validate(payload: { sub: string; email: string; role: string }) {
+    const user = await this.userModel.findById(payload.sub);
+    if (!user) throw new UnauthorizedException();
+    return user;
+  }
+}
