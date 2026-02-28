e91333e795bf265c599e210d15e94fa5fb1f025b
awoladhossain <awoladh04@gmail.com>
2026-03-01 00:50:29 +0600

    category service started

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { Category, CategoryDocument } from './schemas/category.schema';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectModel(Category.name) private categoryModel: Model<CategoryDocument>,
+  ) {}
+  async create(dto: CreateCategoryDto) {
+    const exists = await this.categoryModel.findOne({ name: dto.name });
+    if (exists) {
+      throw new ConflictException('Category already exists');
+    }
+    const category = await this.categoryModel.create(dto);
+    return { message: 'Category created', category };
+  }
+
+  async findAll() {
+    return this.categoryModel.find({ isActive: true });
+  }
+
+  async findOne(id: string) {
+    const category = await this.categoryModel.findById(id);
+    if (!category) throw new NotFoundException('Category not found');
+    return category;
+  }
+}
